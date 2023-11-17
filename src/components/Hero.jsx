44442150e3b048882995d842761153c6ceb23b59
Depreeth <dc3016@srmist.edu.in>
2023-11-18 03:53:49 +0530

    added the 3d animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,12 @@ const Hero = () => {
           <div className=' w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className=' w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className=' text-[#915eff]'>Depreeth</span></h1>
+          <p className={`${styles.heroSubText} mt-3 text-white-100`}>I develop Web Applications</p>
+        </div>
       </div>
+      <ComputersCanvas/>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React,{Suspense, useEffect, useState} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer= useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={1.75} groundColor="black"/>
+      <pointLight intensity={1}/>
+      <primitive object={computer.scene} scale={0.75} position={[0,-3,-1.5]}/>
+    </mesh>
+  )
+}
+const ComputerCanvas = () =>{
+  return(
+    <Canvas frameloop='demand' shadows camera={{position:[20,3,5], fov:25}} gl={{preserveDrawingBuffer: true}}>
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2}/>
+        <Computers/>
+      </Suspense>
+      <Preload all/>
+    </Canvas>
   )
 }
 
-export default Computers
+
+export default ComputerCanvas
