2780ad6740207ce44af83d0955d4388ddee34a3f
Depreeth <dc3016@srmist.edu.in>
2023-11-22 03:32:56 +0530

    done with tech section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,8 +7,9 @@ const Computers = ({isMobile}) => {
   const computer= useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
-      <hemisphereLight intensity={3.75} groundColor="black"/>
+      <hemisphereLight intensity={3} groundColor="black"/>
       <pointLight intensity={1}/>
+      <directionalLight intensity={2}/>
       <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={3} castShadow shadow-mapSize={1024}/>
       <primitive object={computer.scene} scale={isMobile?0.5: 0.75} position={isMobile?[0,-2.75,-1.5]:[0,-3.25,-1.5]} rotation={[-0.01, -0.2,-0.1]}/>
     </mesh>
